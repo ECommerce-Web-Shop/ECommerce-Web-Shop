dada1c7a984f31bc2356018bfd30c103827bb000
phamquankun <phamquankun@gmail.com>
2021-11-01 22:50:45 +0700

    index.js commit

---
+++ package/ec-web/components/index.js
@@ -0,0 +1,23 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Banner from "./Banner/Banner";
+import Modal from "./Modal/Modal";
+import Review from "./Review/Review";
+import About from "./About/About"
+import RatingStars from "./RatingStars/RatingStars";
+import CartItem from "./CartItem/CartItem";
+import Product from './Category/product';
+import Pagination from "./Wishlist/pagination";
+
+export {
+    Header,
+    Footer,
+    Modal,
+    Review,
+    RatingStars,
+    CartItem,
+    Banner,
+    About,
+    Product,
+    Pagination
+};
