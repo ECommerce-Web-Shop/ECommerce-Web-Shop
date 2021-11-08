bfc3ffe78ab013c5153f053f78054fad927ca9eb
Thinhhanghieu <84561393+Thinhhanghieu@users.noreply.github.com>
2021-11-08 20:38:19 +0700

    Fix Product and add Modal

---
+++ package/ec-web/components/ProductList/ProductList.js
@@ -0,0 +1,45 @@
+import React, { useEffect } from "react";
+
+export default function ProductList(props) {
+  useEffect(() => {
+    if (props.isCarousel) {
+      new Flickity(`#${props.id}`, {
+        imagesLoaded: true,
+        freeScroll: true,
+        prevNextButtons: false,
+        pageDots: false,
+        contain: true,
+      });
+    }
+  });
+
+  let productList = props.productsList.map((product, index) => {
+    return (
+      <Product
+        key={index}
+        images={`${Config.API_URL}${product.thumbnail.url}`}
+        sales={product.sales}
+        regularPrice={product.regularPrice}
+        rating={product.rating}
+        votes={product.votes}
+      >
+        {product.name}
+      </Product>
+    );
+  });
+
+  return (
+    <Flickity
+      flickityRef={(c) => (flkty = c)}
+      className={"carousel"} // default ''
+      id={props.id}
+      elementType={"div"} // default 'div'
+      options={flickityOptions} // takes flickity options {}
+      disableImagesLoaded={false} // default false
+      reloadOnUpdate // default false
+      static // default false
+    >
+      {productList}
+    </Flickity>
+  );
+}
